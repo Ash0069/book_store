fe56c92d8ce08659078cc2238206f414d057bd52
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-06-07 03:33:55 +0530

    Forms Frontend started

--- src/books/urls.py
+++ src/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import book_title_view_list, book_title_detail
+from .views import BookTitleListView, book_title_detail
 
 app_name = 'books'
 
 urlpatterns = [
-    path('', book_title_view_list, name='main'),
+    path('', BookTitleListView.as_view(), name='main'),
     path('<pk>/', book_title_detail, name='detail'),
 ]
--- src/books/views.py
+++ src/books/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from .models import Book, BookTitle
-
+from django.views.generic import ListView, FormView
+from .forms import BookTitleForm
+from django.urls import reverse_lazy, reverse
+from django.contrib import messages
 # Create your views here.
-def book_title_view_list(request):
-    qs = BookTitle.objects.all()
-    return render(request, 'books/main.html', { 'qs' : qs })
+
+class BookTitleListView(FormView, ListView):
+    # model = BookTitle
+    queryset = BookTitle.objects.all()
+    template_name = 'books/main.html'
+    context_object_name = 'qs'
+    form_class = BookTitleForm
+    # success_url = reverse_lazy("books:main")
+    # ordering = ('-created',)
+    i_instance = None
+
+    def get_success_url(self):
+        return self.request.path
+
+    def get_queryset(self):
+        parameter = 's'
+        return BookTitle.objects.filter(title__startswith=parameter)
+
+    def form_valid(self, form):
+        self.i_instance = form.save()
+        messages.add_message(self.request, messages.INFO, f"Book Title : {self.i_instance.title} has been created", extra_tags="success")
+        return super().form_valid(form)
+        # form.save()
+        # response = super().form_valid(form)
+        # # Inject flag into context
+        # self.extra_context = {'success': True}
+        # return response
+    
+    def form_invalid(self, form):
+        # self.object_list = self.get_queryset()
+        # return super().form_invalid(form)
+        self.object_list = self.get_queryset()
+        # Optionally, handle specific field errors
+        self.extra_context = {'form': form, 'title_error': form['title'].errors}
+        return super().form_invalid(form)
+    
+# def book_title_view_list(request):
+#     qs = BookTitle.objects.all()
+#     return render(request, 'books/main.html', { 'qs' : qs })
 
 def book_title_detail(request, **kwargs):
     pk=kwargs.get('pk')
