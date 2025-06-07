94a6e19667f7b48a0f435ae6279f0d3a2ffec4dc
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-06-07 23:07:06 +0530

    modal forms

--- src/core/settings.py
+++ src/core/settings.py
@@ -142,6 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'books' / 'static',
+]
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
